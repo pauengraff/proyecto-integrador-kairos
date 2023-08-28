6c8a692079f4961a86ec7f72317983bcd0048fab
GeraZorzoni <gzorzoni@gmail.com>
2023-08-28 09:30:32 -0300

    Agrego Rutas

--- src/app.js
+++ src/app.js
@@ -17,3 +17,16 @@ app.get('/', (req, res) => {
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productDetail.html'));
 });
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productCart.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
