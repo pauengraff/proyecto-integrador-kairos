f1e25fea0b931d31e597c904149da149cfb16ea1
GeraZorzoni <gzorzoni@gmail.com>
2023-11-10 15:18:15 -0300

    continuo trabajo de migracion a SQL de controller y services

--- src/services/products-service-sql.js
+++ src/services/products-service-sql.js
@@ -4,4 +4,21 @@ module.exports = {
   getAllProducts: () => {
     return Product.findAll({ include: ["category", "brand"] });
   },
+
+  /* servicios a crear viendo modelo SQL
+  getProduct: (id) => {
+    const product = db.products.findById(id);
+    return product;
+  },
+  createProduct: (product) => {
+    db.products.create(product);
+  },
+  updateProduct: (id, product) => {
+    console.log(`Updating IN SERVICE product ${product.name}`);
+    db.products.update(id, product);
+  },
+  deleteProduct: (id) => {
+    db.products.delete(id);
+  },
+*/
 };
