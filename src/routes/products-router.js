99b0720852e45a346fb54850fb5cecbebc8349d5
GeraZorzoni <gzorzoni@gmail.com>
2023-11-13 10:59:29 -0300

    Elimino comentarios utilizados en la migracion

--- src/routes/products-router.js
+++ src/routes/products-router.js
@@ -17,32 +17,25 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // ************ Controller Require ************
-//const productsController = require("../controllers/products-controller");
-// Arranco test de vistas con el controlador todo dirigido a SQL
+
 const productsController = require("../controllers/products-controller");
 
 // Rutas products
 //GET ALL PRODUCTS
-//INDEX OK SQL
 router.get("/", productsController.index);
 
 // *** PRODUCT CART ***/
-// CART OK SQL
 router.get("/Cart", productsController.productCart);
 
 /*** CREATE ONE PRODUCT ***/
-// GET OK SQL - falta post
 router.get("/add", productsController.add);
 router.post("/", upload.single("image"), productsController.store);
 
 /*** EDIT PRODUCT ***/
-//Formulario de edición de productos
-// GET OK - Put da error al guarda productos
 router.get("/edit/:id", productsController.edit);
 router.put("/:id", productsController.update);
 
 /*** GET ONE PRODUCT BY ID ***/
-//detailById OK SQL
 router.get("/:id/", productsController.detailById);
 
 /*** DELETE ONE PRODUCT***/
