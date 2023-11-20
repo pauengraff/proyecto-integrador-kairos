3927c8b521d6018ad06e9ee422c380c66323568e
pauengraff <pauengraff@gmail.com>
2023-11-20 10:05:21 -0300

    agrego las mismas rutas que productos pero las dejo comentadas para ir trabajando una por una

--- src/routes/users-router-sql.js
+++ src/routes/users-router-sql.js
@@ -16,10 +16,27 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// ************ Controller Require ************
 const usersControllerSql = require("../controllers/users-controller-sql");
 
+// Rutas usuarios
+//GET ALL USERS
 router.get("/", usersControllerSql.index);
+
+
+/*** CREATE USER ***/
 router.get("/register", usersControllerSql.register);
-router.post("/register", usersControllerSql.create);
+router.post("/", upload.single ("image"), usersControllerSql.create);
+/*router.post("/", upload.single("image"), productsController.store);*/
+
+/*** EDIT USER ***/
+//router.get("/edit/:id", productsController.edit);
+//router.put("/:id", productsController.update);
+
+/*** GET USER BY ID ***/
+//router.get("/:id/", productsController.detailById);
+
+/*** DELETE USER ***/
+//router.delete("/:id", productsController.destroy);
 
 module.exports = router;
