86dcf7ce9dd09f5a967ad08e899ad0fe963097a5
GeraZorzoni <gzorzoni@gmail.com>
2023-11-30 09:30:10 -0300

    creo api UPDATE para editar producto

--- website/src/controllers/api/productsController.js
+++ website/src/controllers/api/productsController.js
@@ -37,4 +37,17 @@ module.exports = {
       data: product,
     });
   },
+
+  //Solo creo Api para Editar producto no para la vista de formulario
+  update: async (req, res) => {
+    await productsService.updateProduct(req.params.id, req.body);
+    res.json({
+      meta: {
+        status: 201,
+        total: product.length,
+        url: req.originalUrl,
+      },
+      data: product,
+    });
+  },
 };
