7df8dad00bd3a344168b95f7fb5b6624c3ab3af5
pauengraff <pauengraff@gmail.com>
2023-12-04 22:56:12 -0300

    creacion de archivos users y usersId

---
+++ dashboard/src/components/Content/UsersId.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+function UsersId({ first_name, last_name, email }) {
+  return (
+    <article className="productitem">
+      <div>{first_name}</div>
+      <div>{last_name}</div>
+      <div>{email}</div>
+    </article>
+  );
+}
+
+UsersId.propTypes = {
+  first_name: PropTypes.string.isRequired,
+};
+
+export default UsersId;
--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import logo from "../assets/images/logo/kairos-logo-color.png";
 import "./Dashboard.css";
 import Products from "./Content/Products";
+import Users from "./Content/Users";
 
 function Dashboard() {
   return (
     <div>
       <Products />
+      <Users />
       <img className="width-20" src={logo}></img>
     </div>
   );
