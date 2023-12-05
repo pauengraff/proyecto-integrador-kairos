6d2e0d7d4abe9db5764cc48050c6835850b8530a
GeraZorzoni <gzorzoni@gmail.com>
2023-12-05 08:54:43 -0300

    trabajo sobre script de navBar

--- website/public/js/navBar.js
+++ website/public/js/navBar.js
@@ -9,10 +9,36 @@
     listElements.forEach((element) => {
       element.addEventListener("click", () => {
         let subMenu = element.children[1];
+        let height = 0;
         element.classList.toggle("menu_item--active");
+
+        if (subMenu.clientHeight === 0) {
+          height = subMenu.scrollHeight;
+        }
+
+        subMenu.style.height = `${height}px`;
       });
     });
   };
 
-  addClick();
+  const deleteStyleheight = () => {
+    listElements.forEach((element) => {
+      if (element.children[1].getAttribute("style")) {
+        element.children[1].removeAttribute("style");
+        element.classList.remove("menu_item--active");
+      }
+    });
+  };
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+      deleteStyleheight();
+    } else {
+      addClick;
+    }
+  });
+
+  if (window.innerWidth <= 800) {
+    addClick();
+  }
 })();
