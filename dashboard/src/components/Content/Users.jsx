f55d44242cdf8d9efd95d3f36616e73245ee1d44
GeraZorzoni <gzorzoni@gmail.com>
2023-12-11 23:01:21 -0300

    Convierto clase Users usando hooks

--- dashboard/src/components/Content/Users.jsx
+++ dashboard/src/components/Content/Users.jsx
@@ -1,49 +1,36 @@
 import UsersId from "./UsersId";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class Users extends Component {
-  constructor(props) {
-    super(props);
+function Users() {
+  const [users, setProducts] = useState([]);
 
-    this.state = {
-      user: [],
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3008/api/user");
+      const result = await response.json();
+      setProducts(result.data);
     };
-  }
+    fetchData();
+  }, []);
 
-  componentDidMount() {
-    fetch("http://localhost:3008/api/user")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        const user = json.data;
-        console.log("user", user);
-        this.setState({
-          user: user,
-        });
-      });
-  }
-
-  render() {
-    return (
-      <section className=''>
-        <h2 className=''>Usuarios</h2>
-        <div className=''>
-          {this.state.user.length === 0
-            ? "Cargando..."
-            : this.state.user.map((user) => (
-                <UsersId
-                  key={user.id}
-                  first_name={user.first_name}
-                  last_name={user.last_name}
-                  email={user.email}
-                  avatar={user.avatar}
-                />
-              ))}
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className=''>
+      <h2 className=''>Usuarios</h2>
+      <div className=''>
+        {users.length === 0
+          ? "Cargando..."
+          : users.map((user) => (
+              <UsersId
+                key={user.id}
+                first_name={user.first_name}
+                last_name={user.last_name}
+                email={user.email}
+                avatar={user.avatar}
+              />
+            ))}
+      </div>
+    </section>
+  );
 }
 
 export default Users;
