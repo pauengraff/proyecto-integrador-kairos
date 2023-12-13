dcbc0723ad6297c187753d088487d55329ce9b5e
GeraZorzoni <gzorzoni@gmail.com>
2023-12-13 14:13:47 -0300

    puse algo en el footer

--- dashboard/src/components/Footer.jsx
+++ dashboard/src/components/Footer.jsx
@@ -1,5 +1,29 @@
 import "./Footer.css";
+import logo from "../assets/images/logo/kairos-logo-blanco.png";
 
 export default function Footer() {
-  return <div className='footer'>Soy el Footer</div>;
+  return (
+    <footer className='footer'>
+      <div className='main-footer'>
+        <div className='left-section-footer'>
+          <div className='logo-footer'>
+            <img src={logo} alt='' />
+          </div>
+
+          <div className='icons-footer'>
+            <i className='fa-brands fa-instagram'></i>
+            <i className='fa-brands fa-square-twitter'></i>
+            <i className='fa-brands fa-facebook'></i>
+            <i className='fa-brands fa-youtube'></i>
+          </div>
+        </div>
+
+        <div className='right-section-footer'>
+          <p>Todo el tiempo para vos</p>
+        </div>
+
+        <p className='copyright'>©Copyright Kairos 2023</p>
+      </div>
+    </footer>
+  );
 }
