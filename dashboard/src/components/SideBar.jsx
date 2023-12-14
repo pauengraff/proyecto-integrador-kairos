1ed6199bb065c12a69ef04eba40f5a3ec9c067e1
GeraZorzoni <gzorzoni@gmail.com>
2023-12-14 12:40:56 -0300

    Configuro estructura de links

--- dashboard/src/components/SideBar.jsx
+++ dashboard/src/components/SideBar.jsx
@@ -3,7 +3,17 @@ import { Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <nav>
-      <Link to=''>Login</Link>
+      <ul>
+        <li>
+          <Link to=''>Login</Link>
+        </li>
+        <li>
+          <Link to=''>Relojes</Link>
+        </li>
+        <li>
+          <Link to=''>Carrito</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
