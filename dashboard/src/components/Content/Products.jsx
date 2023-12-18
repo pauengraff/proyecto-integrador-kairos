7a08ea70990a0cb9dfa0746d15f060744c2fa201
GeraZorzoni <gzorzoni@gmail.com>
2023-12-18 11:08:56 -0300

    Sigo trabajando sobre el orden de los productos al mostrarse

--- dashboard/src/components/Content/Products.jsx
+++ dashboard/src/components/Content/Products.jsx
@@ -17,24 +17,22 @@ function Products() {
   }, []);
 
   return (
-    <section className='section'>
-      <div className='product-container'>
-        <article className='product'>
-          {products.length === 0
-            ? "Cargando..."
-            : products.map((product) => (
-                <ProductItem
-                  key={product.id}
-                  name={product.name}
-                  brand={product.brand.name}
-                  category={product.category.name}
-                  description={product.description}
-                  image={product.image}
-                />
-              ))}
-        </article>
-      </div>
-    </section>
+    <div className='product-container'>
+      <article className='product'>
+        {products.length === 0
+          ? "Cargando..."
+          : products.map((product) => (
+              <ProductItem
+                key={product.id}
+                name={product.name}
+                brand={product.brand.name}
+                category={product.category.name}
+                description={product.description}
+                image={product.image}
+              />
+            ))}
+      </article>
+    </div>
   );
 }
 
