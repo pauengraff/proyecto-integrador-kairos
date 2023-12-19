fbbd2224b9a0f8164f5186d124a4fea6426c92ce
pauengraff <pauengraff@gmail.com>
2023-12-18 21:34:13 -0300

    elimino archivo profile.js

--- website/public/js/profile.js
+++
@@ -1,6 +0,0 @@
-function buttonBuyColor(){
-    let buttonBuy = document.querySelector("buttonBuy");
-    buttonBuy.addEventListener("mouseOver", function(){
-        this.style.color = "#7c92bd";
-    })
-};
