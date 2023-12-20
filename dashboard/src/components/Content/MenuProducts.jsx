89a0f391f4e7a1e102cfa536ad5deaff9d20e1c3
GeraZorzoni <gzorzoni@gmail.com>
2023-12-20 08:18:34 -0300

    creo MenuProducts y comeinzo trabajo sobre links por categorias de productos

---
+++ dashboard/src/components/Content/MenuProducts.jsx
@@ -0,0 +1,4 @@
+import { Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+//import GenreDetail from "./Detalle-genero"; importar detalles de categorias
+//import GenreLink from "./Link-genero"; importarde talles de links a categorias
