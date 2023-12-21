4cb61433da4492fab70b605e6260996b5bcf1916
GeraZorzoni <gzorzoni@gmail.com>
2023-12-21 09:48:49 -0300

    Agrego link de categorias en sideBar de website

--- website/src/controllers/main-controller.js
+++ website/src/controllers/main-controller.js
@@ -1,11 +1,17 @@
+const productsService = require("../services/products-service");
+const categoriesService = require("../services/category-service");
+const brandService = require("../services/brand-service");
+
 module.exports = {
-  index: (req, res) => {
-    res.render("index");
+  index: async (req, res) => {
+    const category = await categoriesService.getAllCategories;
+    const brand = await brandService.getAllBrands;
+    res.render("index"), { category, brand };
   },
   contact: (req, res) => {
     res.render("contact");
   },
   contactMsg: (req, res) => {
-    res.render("contact-msg")
-  }
+    res.render("contact-msg");
+  },
 };
