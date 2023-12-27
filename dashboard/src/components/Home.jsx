9dd82555c2ae009e1d6bdf8a9f589258c4331285
GeraZorzoni <gzorzoni@gmail.com>
2023-12-27 10:24:40 -0300

    Agego foto al home

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+import logoHome from "../assets/images/categories/ImagenHome.jpeg";
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <img src={logoHome} alt='logoHome' />
+    </div>
+  );
+}
--- dashboard/src/components/SideMenuRoutes.jsx
+++ dashboard/src/components/SideMenuRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "react-router-dom";
 import "./SideMenu.css";
+import Home from "./Home";
 import Login from "./Login";
 import Cart from "./Cart";
 import ControlPanel from "./ControlPanel";
@@ -10,6 +11,9 @@ import ProductDetail from "./Content/ProductDetail";
 export default function SideMenuRoutes() {
   return (
     <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
       <Route path='/products'>
         <MenuCategory />
       </Route>
