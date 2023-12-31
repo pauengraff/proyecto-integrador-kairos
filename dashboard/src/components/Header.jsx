f1467d3a1e150be79df5bc95fc73f924b87dd4e1
pauengraff <pauengraff@gmail.com>
2023-12-31 12:49:12 -0300

    borro archivo navbar del header

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
             <li><img src={slider3} alt=""></img></li>
             <li><img src={slider4} alt=""></img></li>
           </ul>
-    </div>
+        </div>
     </div>
   );
 }
