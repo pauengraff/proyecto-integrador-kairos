70855c6a7c2cf617ab44896998b872ba84278d69
pauengraff <pauengraff@gmail.com>
2024-05-20 17:03:33 -0300

    creo el archivo .env y modifico el config con las variables de entorno

--- website/src/database/config/config.js
+++ website/src/database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "kairos_db_V2",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
@@ -14,10 +15,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
 };
